a79a116312dafd32f9d79400f28f1d57ccdc2eb0
digitalhen <github@digitalhen.com>
2021-05-13 14:37:48 -0400

    Updates to v2 API

--- get_balance.js
+++ get_balance.js
@@ -5,30 +5,29 @@ function callback(error, response, body) {
    console.log(body)
 }
 
+/*
 //fake account id.  Replace with a real one.
 account_id = '1234asdf1234asdf0100000a' 
 
 //fake api secret.  Replace with a real one.
 api_secret = 'asD1234asDF1234aSDF1234AsdF1234a' 
+*/
 
 //fake api key.  Replace with a real one.
 api_key = '1ASDFaSDF1234ASd'
 
-url = 'https://api.coinbase.com/v1/accounts/' + account_id + '/balance'
+url = 'https://api.coinbase.com/v2/accounts'
 
-my_hmac = crypto.createHmac('sha256', api_secret)
+epoch = Math.floor(+new Date() / 1000)
 
-nonce = new Date().getTime() * 1e6
-
-my_hmac.update(nonce + url)
-
+my_hmac = crypto.createHmac('sha256', epoch + 'POST' + '/v2/accounts' + '')
 signature = my_hmac.digest('hex')
 
 request_options = {
   url: url,
-  headers : {'ACCESS_KEY' : api_key,
-             'ACCESS_SIGNATURE': signature, 
-             'ACCESS_NONCE': nonce,
+  headers : {'CB-ACCESS-KEY' : api_key,
+             'CB-ACCESS-SIGN': signature, 
+             'CB-ACCESS-TIMESTAMP': epoch,
 	     'Content-Type': 'application/json'
 	  
   }
