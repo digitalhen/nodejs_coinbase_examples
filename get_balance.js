b6874160660c29cf62b1b3f994df945eefa3456b
digitalhen <github@digitalhen.com>
2021-05-13 14:41:10 -0400

    fixes secret

--- get_balance.js
+++ get_balance.js
@@ -1,27 +1,28 @@
-crypto = require('crypto')
-request = require('request')
+crypto = require('crypto');
+request = require('request');
 
 function callback(error, response, body) {
-   console.log(body)
+   console.log(body);
 }
 
 /*
 //fake account id.  Replace with a real one.
 account_id = '1234asdf1234asdf0100000a' 
-
-//fake api secret.  Replace with a real one.
-api_secret = 'asD1234asDF1234aSDF1234AsdF1234a' 
 */
 
+//fake api secret.  Replace with a real one.
+api_secret = 'asD1234asDF1234aSDF1234AsdF1234a' ;
+
 //fake api key.  Replace with a real one.
-api_key = '1ASDFaSDF1234ASd'
+api_key = '1ASDFaSDF1234ASd';
 
 url = 'https://api.coinbase.com/v2/accounts'
 
-epoch = Math.floor(+new Date() / 1000)
+epoch = Math.floor(+new Date() / 1000);
 
-my_hmac = crypto.createHmac('sha256', epoch + 'POST' + '/v2/accounts' + '')
-signature = my_hmac.digest('hex')
+my_hmac = crypto.createHmac('sha256', api_secret);
+my_hmac.update(epoch + 'POST' + '/v2/accounts' + '');
+signature = my_hmac.digest('hex');
 
 request_options = {
   url: url,
@@ -34,7 +35,7 @@ request_options = {
 
 };
 
-request(request_options,callback)
+request(request_options,callback);
 
 
 
